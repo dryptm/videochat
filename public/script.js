42afb658b3ec8b6952414f93d6d90bca3c91a701
Vinaya Kumar <vinay9415756155@gmail.com>
2020-09-12 21:45:51 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,7 +1,53 @@
-const socket=io("/")
-
-socket.emit("join-room",ROOM_ID,10)
-
-socket.on("user-connected",function(userid){
-    console.log("user connected"+userid);
+const socket = io("/")
+const videoGrid = document.getElementById("video-grid")
+const mypeer = new Peer(undefined, {
+    host: "/",
+    port: "3001"
 })
+const myvideo = document.createElement("video")
+//myvideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(function (stream) {
+    addvideostream(myvideo, stream)
+
+    mypeer.on("call",function(call){
+        call.answer(stream)
+        const video=document.createElement("video")
+        call.on("stream",function(uservideostream){
+            addvideostream(video,uservideostream)
+        })
+    })
+
+    socket.on("user-connected", function (userid) {
+        connectToNewUser(userid, stream)
+    })
+
+})
+
+mypeer.on("open", function (id) {
+    socket.emit("join-room", ROOM_ID, id)
+})
+
+function connectToNewUser(userid,stream)
+{
+    const call = mypeer.call(userid,stream)
+    const video=document.createElement("video")
+    call.on("stream",function(uservideostream){
+        addvideostream(uservideostream)
+    })
+    call.on("close",function(){
+        video.remove()
+    })
+}
+
+function addvideostream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener("loadedmetadata", function () {
+        video.play()
+
+    })
+    videoGrid.append(video);
+}
